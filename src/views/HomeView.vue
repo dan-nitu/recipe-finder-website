e66f0e65e898f0af418dfbd0766b2436900501e8
Voodoo30fps <voodoo30fps@gmail.com>
2025-11-05 10:54:48 +0200

    footer - mobile design

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -72,4 +72,20 @@
       <a href="#" class="button">Browse recipes</a>
     </div>
   </section>
+
+  <footer>
+    <div class="social-media-links">
+      <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+        <img src="/src/assets/images/icon-instagram.svg" alt="instagram-icon" />
+      </a>
+      <a href="https://bsky.app/" target="_blank" rel="noopener noreferrer">
+        <img src="/src/assets/images/icon-bluesky.svg" alt="bluesky-icon"
+      /></a>
+      <a href="https://www.tiktok.com/" target="_blank" rel="noopener noreferrer">
+        <img src="/src/assets/images/icon-tiktok.svg" alt="tiktok-icon"
+      /></a>
+    </div>
+
+    <div class="footer-text">Made with ❤️ and 🥑</div>
+  </footer>
 </template>
