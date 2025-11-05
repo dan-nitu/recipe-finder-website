32e76702ebc385819b50b73768156a9673ef34c9
Voodoo30fps <voodoo30fps@gmail.com>
2025-11-05 10:34:36 +0200

    real life section - mobile design

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -48,4 +48,17 @@
       </div>
     </div>
   </section>
+
+  <section class="real-life">
+    <div class="real-life-text">
+      <h2>Built for real life</h2>
+      <p>
+        Cooking shouldn't be complicated. These recipes come in under 30 minutes of active time, fit
+        busy schedules, and taste good enough to repeat.
+      </p>
+      <p>Whether you're new to the kitchen or just need fresh ideas, we've got you covered.</p>
+    </div>
+
+    <img src="/src/assets/images/image-home-real-life-small.webp" alt="man-preparing-food-table" />
+  </section>
 </template>
