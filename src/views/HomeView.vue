a57a56af10545dc0d6269b348d0439f7f110658a
Voodoo30fps <voodoo30fps@gmail.com>
2025-11-05 10:27:12 +0200

    features section - mobile design

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,4 +14,38 @@
 
     <img src="/src/assets/images/image-home-hero-small.webp" alt="" />
   </section>
+
+  <section class="features">
+    <h2>What you'll get</h2>
+
+    <div class="features-list">
+      <div class="feature-item">
+        <div class="image-wrapper">
+          <img src="/src/assets/images/icon-whole-food-recipes.svg" alt="icon-whole-food-recipes" />
+        </div>
+        <div class="feature-text">
+          <h3>Whole-food recipes</h3>
+          <p>Each dish uses everyday, unprocessed ingredients.</p>
+        </div>
+      </div>
+      <div class="feature-item">
+        <div class="image-wrapper">
+          <img src="/src/assets/images/icon-minimum-fuss.svg" alt="icon-whole-food-recipes" />
+        </div>
+        <div class="feature-text">
+          <h3>Minimum fuss</h3>
+          <p>All recipes are designed to make eating healthy quick and easy.</p>
+        </div>
+      </div>
+      <div class="feature-item">
+        <div class="image-wrapper">
+          <img src="/src/assets/images/icon-search-in-seconds.svg" alt="icon-whole-food-recipes" />
+        </div>
+        <div class="feature-text">
+          <h3>Search in seconds</h3>
+          <p>Filter by name or ingredient and jump straight to the recipe you need.</p>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
