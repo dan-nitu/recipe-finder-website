c43859a052f799c7e1f0bf765190d6fc39b7d6a1
Voodoo30fps <voodoo30fps@gmail.com>
2025-11-10 12:41:56 +0200

    added feature item component

---
+++ src/components/FeatureItem.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  imgUrl: String,
+  imgAlt: String,
+  h3: String,
+  paragraph: String,
+})
+</script>
+
+<template>
+  <div class="feature-item">
+    <div class="image-wrapper">
+      <img :src="imgUrl" :alt="imgAlt" />
+    </div>
+    <div class="feature-text">
+      <h3>{{ h3 }}</h3>
+      <p>{{ paragraph }}</p>
+    </div>
+  </div>
+</template>
--- src/components/Features.vue
+++ src/components/Features.vue
@@ -1,35 +1,44 @@
+<script setup>
+import FeatureItem from './FeatureItem.vue'
+
+const featureItems = [
+  {
+    id: 1,
+    imgUrl: '/src/assets/images/icon-whole-food-recipes.svg',
+    imgAlt: 'icon-whole-food-recipes',
+    h3: 'Whole-food recipes',
+    paragraph: 'Each dish uses everyday, unprocessed ingredients.',
+  },
+  {
+    id: 2,
+    imgUrl: '/src/assets/images/icon-minimum-fuss.svg',
+    imgAlt: 'icon-minimum-fuss',
+    h3: 'Minimum fuss',
+    paragraph: 'All recipes are designed to make eating healthy quick and easy.',
+  },
+  {
+    id: 3,
+    imgUrl: '/src/assets/images/icon-search-in-seconds.svg',
+    imgAlt: 'icon-whole-food-recipes',
+    h3: 'Search in seconds',
+    paragraph: 'Filter by name or ingredient and jump straight to the recipe you need.',
+  },
+]
+</script>
+
 <template>
   <section class="features">
     <h2>What you'll get</h2>
 
     <div class="features-list">
-      <div class="feature-item">
-        <div class="image-wrapper">
-          <img src="/src/assets/images/icon-whole-food-recipes.svg" alt="icon-whole-food-recipes" />
-        </div>
-        <div class="feature-text">
-          <h3>Whole-food recipes</h3>
-          <p>Each dish uses everyday, unprocessed ingredients.</p>
-        </div>
-      </div>
-      <div class="feature-item">
-        <div class="image-wrapper">
-          <img src="/src/assets/images/icon-minimum-fuss.svg" alt="icon-whole-food-recipes" />
-        </div>
-        <div class="feature-text">
-          <h3>Minimum fuss</h3>
-          <p>All recipes are designed to make eating healthy quick and easy.</p>
-        </div>
-      </div>
-      <div class="feature-item">
-        <div class="image-wrapper">
-          <img src="/src/assets/images/icon-search-in-seconds.svg" alt="icon-whole-food-recipes" />
-        </div>
-        <div class="feature-text">
-          <h3>Search in seconds</h3>
-          <p>Filter by name or ingredient and jump straight to the recipe you need.</p>
-        </div>
-      </div>
+      <FeatureItem
+        v-for="featureItem in featureItems"
+        :key="featureItem.id"
+        :imgUrl="featureItem.imgUrl"
+        :alt="featureItem.imgAlt"
+        :h3="featureItem.h3"
+        :paragraph="featureItem.paragraph"
+      />
     </div>
   </section>
 </template>
