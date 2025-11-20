80d9b4c0680661cacd70fbc974d1b797ca0e86b5
Voodoo30fps <voodoo30fps@gmail.com>
2025-11-20 11:47:34 +0200

    recipe list and search input vue components

---
+++ src/components/recipes/SearchInput.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  modelValue: String,
+})
+
+const emit = defineEmits(['update:modelValue'])
+</script>
+
+<template>
+  <div class="search-input">
+    <input
+      type="search"
+      id="search"
+      placeholder="Search by name or ingredient..."
+      :value="modelValue"
+      @input="emit('update:modelValue', $event.target.value)"
+    />
+  </div>
+</template>
