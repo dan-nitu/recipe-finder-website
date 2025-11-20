dc824918bbb4c6858b78c40dfc039385e17873cc
Voodoo30fps <voodoo30fps@gmail.com>
2025-11-20 12:39:03 +0200

    recipe suggestions vue component

---
+++ src/components/recipe/MoreRecipes.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { defineProps } from 'vue'
+
+import RecipeCard from '../RecipeCard.vue'
+
+defineProps({
+  recipeSuggestions: {
+    type: Array,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="more-recipes">
+    <h3>More recipes</h3>
+
+    <div class="more-recipes-list recipe-cards">
+      <RecipeCard
+        v-for="recipeSuggestion in recipeSuggestions"
+        :key="recipeSuggestion.id"
+        :imgUrlSmall="'/' + recipeSuggestion.image.small"
+        :imgUrlLarge="'/' + recipeSuggestion.image.large"
+        :imgAlt="recipeSuggestion.title"
+        :title="recipeSuggestion.title"
+        :text="recipeSuggestion.overview"
+        :servings="recipeSuggestion.servings"
+        :prepMinutes="recipeSuggestion.prepMinutes"
+        :cookMinutes="recipeSuggestion.cookMinutes"
+        :url="'/recipes/' + recipeSuggestion.slug"
+      />
+    </div>
+  </div>
+</template>
--- src/views/RecipeView.vue
+++ src/views/RecipeView.vue
@@ -7,6 +7,7 @@ import Breadcrumbs from '@/components/recipe/BreadCrumbs.vue'
 import Details from '@/components/Details.vue'
 import BulletPointList from '@/components/BulletPointList.vue'
 import RecipeCard from '@/components/RecipeCard.vue'
+import MoreRecipes from '@/components/recipe/MoreRecipes.vue'
 
 const route = useRoute()
 const recipes = ref(data)
@@ -71,24 +72,6 @@ watch(
       </div>
     </div>
 
-    <div class="more-recipes">
-      <h3>More recipes</h3>
-
-      <div class="more-recipes-list recipe-cards">
-        <RecipeCard
-          v-for="recipeSuggestion in recipeSuggestions"
-          :key="recipeSuggestion.id"
-          :imgUrlSmall="'/' + recipeSuggestion.image.small"
-          :imgUrlLarge="'/' + recipeSuggestion.image.large"
-          :imgAlt="recipeSuggestion.title"
-          :title="recipeSuggestion.title"
-          :text="recipeSuggestion.overview"
-          :servings="recipeSuggestion.servings"
-          :prepMinutes="recipeSuggestion.prepMinutes"
-          :cookMinutes="recipeSuggestion.cookMinutes"
-          :url="'/recipes/' + recipeSuggestion.slug"
-        />
-      </div>
-    </div>
+    <MoreRecipes :recipeSuggestions="recipeSuggestions" />
   </section>
 </template>
