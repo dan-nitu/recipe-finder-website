9dd4b15626edb406e46b8b2419a349e84d960bc0
Voodoo30fps <voodoo30fps@gmail.com>
2025-11-20 15:49:48 +0200

    updated no results component to include 0 minutes on filters

--- src/components/recipes/NoResults.vue
+++ src/components/recipes/NoResults.vue
@@ -12,10 +12,10 @@ defineProps({
   <div class="no-results">
     <p><b>No recipes found.</b></p>
 
-    <div v-if="maxPrepTime || maxCookTime || searchQuery">
+    <div v-if="maxPrepTime !== null || maxCookTime !== null || searchQuery">
       <p>Filters applied:</p>
-      <div class="filter" v-if="maxPrepTime">Prep ≤ {{ maxPrepTime }} mins</div>
-      <div class="filter" v-if="maxCookTime">Cook ≤ {{ maxCookTime }} mins</div>
+      <div class="filter" v-if="maxPrepTime !== null">Prep ≤ {{ maxPrepTime }} mins</div>
+      <div class="filter" v-if="maxCookTime !== null">Cook ≤ {{ maxCookTime }} mins</div>
       <div class="filter" v-if="searchQuery">Search: "{{ searchQuery }}"</div>
     </div>
 
