84cbefb2b02d786300b08b4de45706afe66f32e5
Voodoo30fps <voodoo30fps@gmail.com>
2025-11-20 12:30:54 +0200

    added scroll to top when the view changes

--- src/router/index.js
+++ src/router/index.js
@@ -4,30 +4,28 @@ import AboutView from '../views/AboutView.vue'
 import RecipesView from '../views/RecipesView.vue'
 import RecipeView from '@/views/RecipeView.vue'
 
+const routes = [
+  { path: '/', name: 'home', component: HomeView },
+  { path: '/about', name: 'about', component: AboutView },
+  { path: '/recipes', name: 'recipes', component: RecipesView },
+  { path: '/recipes/:slug', name: 'recipe', component: RecipeView },
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: AboutView,
-    },
-    {
-      path: '/recipes',
-      name: 'recipes',
-      component: RecipesView,
-    },
-    {
-      path: '/recipes/:slug',
-      name: 'recipe',
-      component: RecipeView,
-    },
-  ],
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      // Back/forward buttons
+      return savedPosition
+    } else if (to.hash) {
+      // Anchor links
+      return { el: to.hash, behavior: 'smooth' }
+    } else {
+      // Default scroll to top
+      return { top: 0 }
+    }
+  },
 })
 
 export default router
