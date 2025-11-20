60e35d7d0f1a356f9cbbd62d120e025bd3f98512
Voodoo30fps <voodoo30fps@gmail.com>
2025-11-20 16:15:38 +0200

    lazy load routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
-import RecipesView from '../views/RecipesView.vue'
-import RecipeView from '@/views/RecipeView.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const routes = [
   { path: '/', name: 'home', component: HomeView },
-  { path: '/about', name: 'about', component: AboutView },
-  { path: '/recipes', name: 'recipes', component: RecipesView },
-  { path: '/recipes/:slug', name: 'recipe', component: RecipeView },
+  { path: '/about', name: 'about', component: () => import('@/views/AboutView.vue') },
+  { path: '/recipes', name: 'recipes', component: () => import('@/views/RecipesView.vue') },
+  { path: '/recipes/:slug', name: 'recipe', component: () => import('@/views/RecipeView.vue') },
 ]
 
 const router = createRouter({
